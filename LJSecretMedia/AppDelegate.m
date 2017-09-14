99198cfe2592124b96574825cb4dd772b7515eb5
lijie <rd32@ce-link.com>
2017-09-14 16:58:46 +0800

    .

--- LJSecretMedia/AppDelegate.m
+++ LJSecretMedia/AppDelegate.m
@@ -41,7 +41,7 @@
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05*NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self show];
-        [KMCGeigerCounter sharedGeigerCounter].enabled = YES;
+//        [KMCGeigerCounter sharedGeigerCounter].enabled = YES;
     });
     return YES;
 }
